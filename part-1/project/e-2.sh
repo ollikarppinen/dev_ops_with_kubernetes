c83e81c10fab8468922c5194e1ad90f6f36f31ad
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-04 20:41:28 +0300

    Renames dir

--- part-1/project/e-2.sh
+++ part-1/project/e-2.sh
@@ -7,7 +7,7 @@ docker push ollikarppinen/todo-1
 kubectl create deployment todo-1 --image=ollikarppinen/todo-1
 
 sleep 20
-kubectl logs deployment/todo-1 >> logs.txt
-kubectl get pods >> pods.txt
+kubectl logs deployment/todo-1 >> e-2-logs.txt
+kubectl get pods >> e-2-pods.txt
 
 kubectl delete deployment todo-1
