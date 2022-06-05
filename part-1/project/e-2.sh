691b8d8f1a518d29ac7071c5685ff149e2f500d0
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-05 11:27:53 +0300

    Renames todo deployment

--- part-1/project/e-2.sh
+++ part-1/project/e-2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker build --tag todo-1 .
-docker tag todo-1 ollikarppinen/todo-1
-docker push ollikarppinen/todo-1
+docker build --tag todo .
+docker tag todo ollikarppinen/todo
+docker push ollikarppinen/todo
 
-kubectl create deployment todo-1 --image=ollikarppinen/todo-1
+kubectl create deployment todo --image=ollikarppinen/todo
 
 sleep 20
-kubectl logs deployment/todo-1 >> e-2-logs.txt
+kubectl logs deployment/todo >> e-2-logs.txt
 kubectl get pods >> e-2-pods.txt
 
-kubectl delete deployment todo-1
+kubectl delete deployment todo
--- part-1/project/e-4.sh
+++ part-1/project/e-4.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f manifests/deployment.yaml
 sleep 10
-kubectl logs deployment/todo-1 >> e-4-logs.txt
+kubectl logs deployment/todo >> e-4-logs.txt
 kubectl get pods >> e-4-pods.txt
-kubectl delete deployment todo-1
+kubectl delete deployment todo
