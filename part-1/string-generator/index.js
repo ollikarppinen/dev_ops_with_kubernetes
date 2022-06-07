e22384162316a6975463e405869bf58d47b7b2df
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-07 22:52:30 +0300

    Renames log-output to string-generator

--- part-1/string-generator/index.js
+++ part-1/string-generator/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const UUID = uuidv4();
-setInterval(() => console.log(`${new Date().toISOString()}: ${UUID}`), 5000);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/status", (request, response) => {
+  const body = {
+    timestamp: new Date().toISOString(),
+    uuid: UUID,
+  };
+  response.json(JSON.stringify(body));
+});
 
 app.listen(PORT, () =>
   console.log(`Express server currently running on port ${PORT}`)
