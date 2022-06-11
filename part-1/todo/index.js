0c99fa7fad0330605bbf6e7187ea07127ec9bda3
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-11 20:47:47 +0300

    Adds basic image html page

--- part-1/todo/index.js
+++ part-1/todo/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.json("FOOBAR");
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(PORT, () =>
