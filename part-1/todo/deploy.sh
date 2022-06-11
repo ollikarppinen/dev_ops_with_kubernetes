eeae165e60c47311337ee2e364969662c33565f1
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-11 23:39:48 +0300

    Adds exercise 1.12 solution

--- part-1/todo/deploy.sh
+++ part-1/todo/deploy.sh
@@ -6,3 +6,4 @@ docker push ollikarppinen/todo
 
 kubectl delete deployments.apps/todo-dep
 kubectl apply -f manifests/
+kubectl logs deployments/todo-dep -f
--- part-1/todo/index.js
+++ part-1/todo/index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const fetch = require("node-fetch");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const IMAGE_URL = "https://picsum.photos/1200";
+const PERSISTED_IMAGE_PATH =
+  process.env.PERSISTED_IMAGE_PATH ||
+  path.join(__dirname, "/downloadedImage.jpg");
 
-app.get("/", function (req, res) {
+const downloadImage = async () => {
+  const res = await fetch(IMAGE_URL);
+  const fileStream = fs.createWriteStream(PERSISTED_IMAGE_PATH);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+};
+
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/image.jpg", async (req, res) => {
+  try {
+    if (!fs.existsSync(PERSISTED_IMAGE_PATH)) await downloadImage();
+    res.sendFile(PERSISTED_IMAGE_PATH);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(PORT, () =>
