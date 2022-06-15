858bd26065662ded93f0031b99af2a6b35773920
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-15 23:58:58 +0300

    Adds todo get and create endpoints

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const { matchedData, body, validationResult } = require("express-validator");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,12 +11,24 @@ app.use(morgan("tiny"));
 
 let todos = [];
 
-app.get("/api/todos", async (request, response) => {
-  response.json(todos);
-});
+app.post(
+  "/api/todos",
+  body("description").notEmpty().isString(),
+  async (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    const bodyData = matchedData(request, { locations: ["body"] });
+    const newTodo = { uuid: uuidv4(), ...bodyData };
+    todos.push(newTodo);
 
-app.post("/api/todos", async (request, response) => {
-  response.json({});
+    response.json(newTodo);
+  }
+);
+
+app.get("/api/todos", async (request, response) => {
+  response.json({ todos });
 });
 
 app.listen(PORT, () =>
