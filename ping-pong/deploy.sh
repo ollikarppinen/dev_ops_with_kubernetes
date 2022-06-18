114fd373a1134c7defa3bb5648abd7ca477bb496
Olli Karppinen <ollikarppinen@gmail.com>
2022-06-18 23:23:48 +0300

    Adds log psql

--- ping-pong/deploy.sh
+++ ping-pong/deploy.sh
@@ -5,4 +5,8 @@ docker tag ping-pong ollikarppinen/ping-pong
 docker push ollikarppinen/ping-pong
 
 kubectl delete deployments.apps/ping-pong-dep
-kubectl apply -f manifests/
+kubectl apply -f manifests/deployment.yaml
+kubectl apply -f manifests/service.yaml
+
+export SOPS_AGE_KEY_FILE=../key.txt
+sops --decrypt manifests/secret.enc.yaml | kubectl apply -f -
