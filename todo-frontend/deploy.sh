6ab6438113582beef12c279ae382a3a2e91ddcb3
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-13 20:48:41 +0300

    Fixes gke deployment

--- todo-frontend/deploy.sh
+++ todo-frontend/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build --push --tag ollikarppinen/todo-frontend --platform linux/arm/v7,linux/arm64/v8,linux/amd64 .
+docker buildx build --push --tag gcr.io/dev-ops-with-kubernetes/todo-frontend --platform linux/arm/v7,linux/arm64/v8,linux/amd64 .
 
 kubectl apply -k .
