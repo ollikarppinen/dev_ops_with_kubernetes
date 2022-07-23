4b70da7baa7b547e3dd421509aac0f57ae539836
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-23 21:15:25 +0300

    Converts todo project to k3d

--- k3d/create-cluster.sh
+++ k3d/create-cluster.sh
@@ -2,4 +2,5 @@
 
 k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 
-docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube
+docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/todo
+docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/log
