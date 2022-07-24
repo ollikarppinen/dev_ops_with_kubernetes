834a9727d9768ed46a724a0d7264df4a084e0e51
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-24 18:16:05 +0300

    Adds todo complete button

--- todo-frontend/deploy.sh
+++ todo-frontend/deploy.sh
@@ -8,7 +8,10 @@ else
   # docker buildx build --push --tag ollikarppinen/todo-frontend --platform linux/arm/v7,linux/arm64/v8,linux/amd64 .
   docker build --tag todo-frontend .
   docker tag todo-frontend ollikarppinen/todo-frontend
-  docker push ollikarppinen/todo-frontend
+  image=ollikarppinen/todo-frontend:"$(git rev-parse --short HEAD)"
+  docker tag todo-backend "$image"
+  docker push "$image"
+  kustomize edit set image PROJECT/IMAGE="$image"
 fi
 
 kubectl apply -k .
