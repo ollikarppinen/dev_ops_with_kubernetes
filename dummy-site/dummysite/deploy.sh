0c21bc8a7d6e6621cfbf938a4e938cfb6f46192b
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-27 20:10:48 +0300

    Removes unnecessary build arg

--- dummy-site/dummysite/deploy.sh
+++ dummy-site/dummysite/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --tag dummysite --build-arg DUMMY_SITE_URL="${DUMMY_SITE_URL}" .
+docker build --tag dummysite .
 docker tag dummysite ollikarppinen/dummysite
 
 docker run -p 3000:3000 dummysite
