103e3d4005505b1b4c1d9600ca495450891cc18d
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-28 22:38:24 +0300

    Replaces the build script with deploy

--- dummy-site/controller/deploy.sh
+++ dummy-site/controller/deploy.sh
@@ -3,3 +3,6 @@
 docker build --tag dummysite-controller .
 docker tag dummysite-controller ollikarppinen/dummysite-controller
 docker push ollikarppinen/dummysite-controller
+
+kubectl delete deployments.apps dummysite-controller-dep
+kubectl apply -f ../manifests/deployment.yaml
