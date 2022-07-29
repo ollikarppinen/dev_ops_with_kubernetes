d2d4a6a31b22b195dd1a82dd16e1dc96ea3e0cfa
Olli Karppinen <ollikarppinen@gmail.com>
2022-07-29 22:26:33 +0300

    Adds working controller code

--- dummy-site/controller/index.js
+++ dummy-site/controller/index.js
@@ -1,206 +1,80 @@
 const k8s = require("@kubernetes/client-node");
-const mustache = require("mustache");
-const request = require("request");
-const JSONStream = require("json-stream");
 const fs = require("fs").promises;
 
-console.log("Kube setup");
+const MYCUSTOMRESOURCE_GROUP = "foo.bar";
+const MYCUSTOMRESOURCE_VERSION = "v1";
+const MYCUSTOMRESOURCE_PLURAL = "dummysites";
+const NAMESPACE = "default";
 
 const kc = new k8s.KubeConfig();
-
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 process.env.NODE_ENV === "development"
   ? kc.loadFromDefault()
   : kc.loadFromCluster();
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
 
-const opts = {};
+const watch = new k8s.Watch(kc);
 
-const sendRequestToApi = async (api, method = "get", options = {}) =>
-  new Promise((resolve, reject) =>
-    request[method](
-      `${kc.getCurrentCluster().server}${api}`,
-      { ...opts, ...options, headers: { ...options.headers, ...opts.headers } },
-      (err, res) => (err ? reject(err) : resolve(JSON.parse(res.body)))
-    )
-  );
+const getDeploymentObject = async (obj) => {
+  const deploymentTemplate = await fs.readFile("deployment.json", "utf-8");
+  const newDeployment = JSON.parse(deploymentTemplate);
 
-const fieldsFromDummySite = (object) => {
-  if (!object) return {};
-  return {
-    dummysite_name: object.metadata.name,
-    container_name: object.metadata.name,
-    deployment_name: `${object.metadata.name}-deployment`,
-    namespace: object.metadata.namespace,
-    website_url: object.spec.website_url,
-  };
+  newDeployment.metadata.name = `${obj.metadata.name}-deployment`;
+  newDeployment.metadata.namespace = obj.metadata.namespace;
+  newDeployment.spec.selector.matchLabels["app"] = obj.metadata.name;
+  newDeployment.spec.template.metadata.labels["app"] = obj.metadata.name;
+  newDeployment.spec.template.spec.containers[0]["name"] = obj.metadata.name;
+  newDeployment.spec.template.spec.containers[0].env[0]["value"] =
+    obj.spec.website_url;
+
+  return newDeployment;
 };
 
-const renderDeploymentYaml = async (fields) => {
-  const deploymentTemplate = await fs.readFile("deployment.mustache", "utf-8");
-  return mustache.render(deploymentTemplate, fields);
+const createDeployment = async (obj) => {
+  const deployment = await getDeploymentObject(obj);
+  console.log("Creating deployment:", deployment);
+  k8sApi.createNamespacedDeployment(NAMESPACE, deployment);
 };
 
-// const exampleFields = {
-//   dummysite_name: "dummysite-hs",
-//   container_name: "dummysite-hs",
-//   deployment_name: `dummysite-hs-deployment`,
-//   namespace: "default",
-//   website_url: "https://hs.fi",
-// };
-
-// renderDeploymentYaml(exampleFields);
-
-// const fieldsFromDeployment = (object) => ({
-//   dummysite_name: object.metadata.labels.app,
-//   container_name: object.metadata.labels.app,
-//   deployment_name: `${object.metadata.labels.app}-deployment`,
-//   namespace: object.metadata.namespace,
-// });
-
-// kc.applyToRequest(opts);
-
-const createDeployment = async (fields) => {
-  console.log(
-    "Creating new deployment for website: ",
-    fields.length,
-    "to namespace",
-    fields.namespace
-  );
-
-  const yaml = await getJobYAML(fields);
-
-  return sendRequestToApi(
-    `/apis/batch/v1/namespaces/${fields.namespace}/jobs`,
-    "post",
-    {
-      headers: {
-        "Content-Type": "application/yaml",
-      },
-      body: yaml,
-    }
+const deleteResource = (obj) => {
+  console.log(`Deleted ${obj.metadata.name}`);
+  return k8sApi.deleteNamespacedDeployment(
+    `${obj.metadata.name}-deployment`,
+    NAMESPACE
   );
 };
 
-const removeJob = async ({ namespace, job_name }) => {
-  const pods = await sendRequestToApi(`/api/v1/namespaces/${namespace}/pods/`);
-  pods.items
-    .filter((pod) => pod.metadata.labels["job-name"] === job_name)
-    .forEach((pod) => removePod({ namespace, pod_name: pod.metadata.name }));
-
-  return sendRequestToApi(
-    `/apis/batch/v1/namespaces/${namespace}/jobs/${job_name}`,
-    "delete"
-  );
-};
-
-const removeCountdown = ({ namespace, countdown_name }) =>
-  sendRequestToApi(
-    `/apis/foo.bar/v1/namespaces/${namespace}/countdowns/${countdown_name}`,
-    "delete"
-  );
-
-const removePod = ({ namespace, pod_name }) =>
-  sendRequestToApi(
-    `/api/v1/namespaces/${namespace}/pods/${pod_name}`,
-    "delete"
-  );
-
-const run = async () => {
-  console.log("Run 1");
-  opts.headers = (
-    await k8sApi.listPodForAllNamespaces()
-  ).response.request.headers;
-
-  /**
-   * Watch Countdowns
-   */
-
-  const dummySiteStream = new JSONStream();
-
-  dummySiteStream.on("data", async ({ type, object }) => {
-    console.log("dummySiteStream received data");
-    console.log(type, object);
-
-    const fields = fieldsFromDummySite(object);
-    console.log(fields);
-
-    if (type === "ADDED") {
-      console.log("Create deployment");
-      // if (await jobForCountdownAlreadyExists(fields)) return; // Restarting application would create new 0th jobs without this check
-      // createJob(fields);
-    }
-    if (type === "DELETED") {
-      console.log("Delete deployment");
-      // cleanupForCountdown(fields);
-    }
-  });
-
-  try {
-    const url = `${
-      kc.getCurrentCluster().server
-    }/apis/foo.bar/v1/dummysites?watch=true`;
-
-    console.log("Connecting", { url, opts });
-
-    request.get(url, opts).pipe(dummySiteStream);
-  } catch (err) {
-    console.log(err);
+const handleEvent = async (type, apiObj) => {
+  console.log(`Received event in type ${type}.`);
+  console.log(apiObj);
+  if (type == "ADDED") {
+    await createDeployment(apiObj);
+  } else if (type == "DELETED") {
+    await deleteResource(apiObj);
+  } else {
+    console.log(`Unknown event type: ${type}`);
   }
-
-  /**
-   * Watch Jobs
-   */
-
-  // const deploymentStream = new JSONStream();
-
-  // deploymentStream.on("data", async ({ type, object }) => {
-  //   console.log("deploymentStream received data");
-  //   console.log(type, object);
-
-  //   if (!object.metadata.labels.website_url) return;
-  //   if (type === "DELETED" || object.metadata.deletionTimestamp) return;
-  //   if (!object?.status?.succeeded) return;
-
-  //   rescheduleJob(object);
-  // });
-
-  // request
-  //   .get(
-  //     `${kc.getCurrentCluster().server}/apis/batch/v1/deployments?watch=true`,
-  //     opts
-  //   )
-  //   .pipe(deploymentStream);
 };
 
-run();
+const onDone = (err) => {
+  console.log(`Connection closed. ${err}`);
+  watchResource();
+};
 
-setInterval(() => console.log("Something failed, sleeping..."), 10000);
+const watchResource = async () => {
+  console.log("Watching resource");
+  return watch.watch(
+    `/apis/${MYCUSTOMRESOURCE_GROUP}/${MYCUSTOMRESOURCE_VERSION}/namespaces/${NAMESPACE}/${MYCUSTOMRESOURCE_PLURAL}`,
+    {},
+    handleEvent,
+    onDone
+  );
+};
 
-// const k8s = require("@kubernetes/client-node");
-// const request = require("request");
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+});
 
-// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const main = async () => await watchResource();
 
-// const kc = new k8s.KubeConfig();
-// kc.loadFromCluster();
-
-// const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
-// var podsResult;
-// k8sApi.listPodForAllNamespaces().then((res) => (podsResult = res));
-
-// const opts = { headers: podsResult.response.request.headers };
-
-// const sendRequestToApi = async (api, method = "get", options = {}) =>
-//   new Promise((resolve, reject) =>
-//     request[method](
-//       `${kc.getCurrentCluster().server}${api}`,
-//       { ...opts, ...options, headers: { ...options.headers, ...opts.headers } },
-//       (err, res) => (err ? reject(err) : resolve(JSON.parse(res.body)))
-//     )
-//   );
-
-// var response;
-// sendRequestToApi("/apis/foo.bar/v1/dummysites").then((res) => (response = res));
+main();
